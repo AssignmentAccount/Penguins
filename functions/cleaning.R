de6007b3764e0ca0e24c03b3349eabf60e4f8e7f
AssignmentAccount <119875487+AssignmentAccount@users.noreply.github.com>
2022-12-08 19:32:16 +0000

    Delete functions directory

--- functions/cleaning.R
+++
@@ -1,9 +0,0 @@
-cleaning <- function(data) {
-  
-  data %>%
-    clean_names() %>%
-    select(-"comments") %>%
-    select(-starts_with("delta")) %>%
-    remove_empty(c("rows","cols"))
-  
-}
--- functions/penguinFunctions.R
+++
@@ -1,7 +0,0 @@
-remove_empty_flipLength <- function(data) {
-  
-  data %>%
-    filter(!is.na(flipper_length_mm)) %>%
-    select(species, flipper_length_mm)
-  
-}
